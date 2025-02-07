5626acc7da8d81fc70a725a5e66098bc954ca2ca
kankan-web <2728360489@qq.com>
2025-02-07 10:43:25 +0800

    feat:react-child基础页面搭建完成

--- packages/qiankun-demo/react-child/src/index.tsx
+++ packages/qiankun-demo/react-child/src/index.tsx
@@ -1,9 +1,40 @@
 import './reset.css'
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import reportWebVitals from './reportWebVitals'
+import { ConfigProvider, Spin } from 'antd'
+import { RouterProvider } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import router from '@/router'
+import { store } from '@/stores/store'
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>Hello</React.StrictMode>)
+const container = document.getElementById('root')!
+
+const root = ReactDOM.createRoot(container)
+root.render(
+	<React.StrictMode>
+		<Provider store={store}>
+			<ConfigProvider
+				prefixCls="ar4"
+				getPopupContainer={node => {
+					if (node) {
+						return node.parentNode as HTMLElement
+					}
+					return container
+				}}
+			>
+				<Suspense
+					fallback={
+						<Spin>
+							<div style={{ width: '100%', height: '200px' }}></div>
+						</Spin>
+					}
+				>
+					<RouterProvider router={router} />
+				</Suspense>
+			</ConfigProvider>
+		</Provider>
+	</React.StrictMode>
+)
 
 reportWebVitals()
